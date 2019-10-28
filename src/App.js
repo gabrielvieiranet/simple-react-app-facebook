cc33eb4a7769990fca711f7f7ed3813fb2241cfd
gabriel <forrest10ster@gmail.com>
2019-10-28 20:55:19 -0300

    subindo desafio04

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import PostList from './components/PostList';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PostList />
+    </>
+  );
+}
+
+export default App;
